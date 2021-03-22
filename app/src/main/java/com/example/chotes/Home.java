5141f0a7dfa504f542a4492b0311d57f28174557
Unknowening <54330569+Unknowening@users.noreply.github.com>
2021-03-21 23:59:53 -0400

    Added Some Appearance Content

--- app/src/main/java/com/example/chotes/Home.java
+++ app/src/main/java/com/example/chotes/Home.java
@@ -1,5 +1,6 @@
 package com.example.chotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +61,18 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        final Button bFind = (Button) view.findViewById(R.id.FindButton);
+
+        bFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Home_Categories.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/chotes/Home_Categories.java
@@ -0,0 +1,19 @@
+package com.example.chotes;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home_Categories extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__catagories);
+
+
+    }
+}
